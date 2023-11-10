c6b45853d24270db1ad732911659cd9ec60fc2d9
Mr-iMikeyyy <mikesawesomevids@gmail.com>
2023-11-10 17:57:58 -0500

    Update tabview.py

--- tabview.py
+++ tabview.py
@@ -8,25 +8,37 @@ class Tabview(customtkinter.CTkTabview):
         super().__init__(*args)
 
         # Create Tabs
-        appTab = self.add("Set Appointment")
-        calTab = self.add("Calendar")
-        confTab = self.add("Configure")
+        self.appTab = self.add("Set Appointment")
+        self.calTab = self.add("Calendar")
+        self.confTab = self.add("Configure")
 
         # Set default active tab
         self.set("Set Appointment")
 
         # Configure Columns of grid
-        # self.tab("Create Post").columnconfigure(0, weight=2)
-        # self.tab("Create Post").columnconfigure(1, weight=2)
-        # self.tab("Create Post").columnconfigure(2, weight=2)
+        self.tab("Set Appointment").columnconfigure(0, weight=1)
+        self.tab("Set Appointment").columnconfigure(1, weight=3)
+
+        self.nameLabel = customtkinter.CTkLabel(self.appTab, text="Name:").grid(column=0, row=0)
+        self.nameEntry = customtkinter.CTkEntry(self.appTab, placeholder_text="Name").grid(column=1, row=0)
+
+        self.carLabel = customtkinter.CTkLabel(self.appTab, text="Car:")
+        self.carLabel.grid(column=0, row=1)
+        self.carEntry = customtkinter.CTkEntry(self.appTab, placeholder_text="Car")
+        self.carEntry.grid(column=1, row=1)
+
+        self.descLabel = customtkinter.CTkLabel(self.appTab, text="Description:")
+        self.descLabel.grid(column=0, row=2)
+        self.descEntry = customtkinter.CTkEntry(self.appTab, placeholder_text="Description")
+        self.descEntry.grid(column=1, row=2)
 
         #Configure Rows of Grid
         # self.tab("Create Post").rowconfigure(0, weight=2)
         # self.tab("Create Post").rowconfigure(1, weight=2)
         # self.tab("Create Post").rowconfigure(2, weight=2)
 
-        self.cal = MplCalendar(date.today().year, date.today().month)
-        self.f = self.cal.getF()
-        self.canvas = FigureCanvasTkAgg(self.f, master=calTab)
-        self.canvas.get_tk_widget().pack()
-        self.canvas.draw()
+        # self.cal = MplCalendar(date.today().year, date.today().month)
+        # self.f = self.cal.getF()
+        # self.canvas = FigureCanvasTkAgg(self.f, master=calTab)
+        # self.canvas.get_tk_widget().pack()
+        # self.canvas.draw()
